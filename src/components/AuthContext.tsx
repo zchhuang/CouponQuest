81845365e0320c511592daaddebc0c94d69ba500
zchhuang <zach9040@berkeley.edu>
2024-08-22 15:31:13 -0700

    remove lint

--- src/components/AuthContext.tsx
+++ src/components/AuthContext.tsx
@@ -36,7 +36,6 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=${responseType}&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&access_type=${accessType}`;
   
     if (isChromeExtension()) {
-         
         chrome.tabs.create({ url: authUrl });
     } else {
         window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?response_type=${responseType}&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&access_type=${accessType}`;
